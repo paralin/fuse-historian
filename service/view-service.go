3ea7c1567b77f7976491cdf341b806a68a2f47fc
Christian Stewart <christian@paral.in>
2016-10-18 22:24:28 -0400

    Drastically simplify view service with common code

--- service/view-service.go
+++ service/view-service.go
@@ -3,11 +3,10 @@ package service
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
-	"time"
 
 	"github.com/fuserobotics/historian"
 	"github.com/fuserobotics/historian/dbproto"
+	"github.com/fuserobotics/reporter/history"
 	"github.com/fuserobotics/reporter/util"
 	"github.com/fuserobotics/reporter/view"
 	"github.com/fuserobotics/statestream"
@@ -125,118 +124,5 @@ func (h *HistorianViewService) GetStateHistory(req *view.StateHistoryRequest, sr
 		return err
 	}
 
-	strm := streame.StateStream
-	var cursor *stream.Cursor
-
-	// first send the history
-	earlyBound := util.NumberToTime(req.Query.BeginTime)
-	lateBound := util.NumberToTime(req.Query.EndTime)
-	if req.Query.EndTime == 0 {
-		lateBound = time.Now()
-	}
-
-	cursor = strm.BuildCursor(stream.ReadForwardCursor)
-	ch := make(chan *stream.StreamEntry)
-	sub := cursor.SubscribeEntries(ch)
-	// spawn a goroutine to send the data, while simultaneously fast forwarding
-	go func() {
-		for {
-			select {
-			case <-srvstream.Context().Done():
-				return
-			case entry, ok := <-ch:
-				if !ok {
-					return
-				}
-				jsonData, err := json.Marshal(entry.Data)
-				if err != nil {
-					break
-				}
-				// not sure what to do if we get an error here.
-				srvstream.Send(&view.StateHistoryResponse{
-					Status: view.StateHistoryResponse_HISTORY_INITIAL_SET,
-					State: &view.StateEntry{
-						JsonState: string(jsonData),
-						Timestamp: util.TimeToNumber(entry.Timestamp),
-						Type:      int32(entry.Type),
-					},
-				})
-			}
-		}
-	}()
-
-	if err := cursor.Init(earlyBound); err != nil {
-		if err == stream.NoDataError {
-			// Check if we can get something after it.
-			entry, err := streame.GetEntryAfter(earlyBound, stream.StreamEntrySnapshot)
-			if err != nil {
-				return err
-			}
-			if entry == nil || entry.Timestamp.After(lateBound) {
-				return stream.NoDataError
-			}
-			earlyBound = entry.Timestamp
-			if err := cursor.InitWithSnapshot(entry); err != nil {
-				return err
-			}
-			// Send the initial snapshot entry
-			ch <- entry
-		} else {
-			return err
-		}
-	}
-
-	cursor.SetTimestamp(lateBound)
-	if err := cursor.ComputeState(); err != nil {
-		return err
-	}
-
-	// close the history channel
-	sub.Unsubscribe()
-	close(ch)
-
-	// if we need to tail, grab the write cursor.
-	if !req.Query.Tail {
-		return nil
-	}
-
-	cursor, err = streame.StateStream.WriteCursor()
-	if err != nil {
-		return err
-	}
-	if !cursor.Ready() {
-		return errors.New("Try again.")
-	}
-
-	// Signal to the client the initial set is done
-	if err := srvstream.Send(&view.StateHistoryResponse{Status: view.StateHistoryResponse_HISTORY_TAIL}); err != nil {
-		return err
-	}
-
-	// Buffer this one, to make sure we don't slow down writing.
-	wch := make(chan *stream.StreamEntry, 100)
-	sub = cursor.SubscribeEntries(wch)
-	defer func() {
-		sub.Unsubscribe()
-	}()
-
-	for {
-		select {
-		case ent := <-wch:
-			jsonData, err := json.Marshal(ent.Data)
-			if err != nil {
-				return err
-			}
-			srvstream.Send(&view.StateHistoryResponse{
-				Status: view.StateHistoryResponse_HISTORY_TAIL,
-				State: &view.StateEntry{
-					JsonState: string(jsonData),
-					Timestamp: util.TimeToNumber(ent.Timestamp),
-					Type:      int32(ent.Type),
-				},
-			})
-		case <-srvstream.Context().Done():
-			return nil
-		}
-	}
+	return history.HandleStateHistoryRequest(req, srvstream, streame.StateStream)
 }
